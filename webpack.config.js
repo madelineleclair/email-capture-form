2792623bcf2560d08507173658d97ce518443027
Madeline LeClair <madelineleclair@gmail.com>
2019-12-02 17:19:40 -0500

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
